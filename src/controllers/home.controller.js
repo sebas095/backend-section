9d6b06ce273127bf4788961fb47cf5ec941a256a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-02-25 00:52:21 -0500

    [ADD] first steps added

---
+++ src/controllers/home.controller.js
@@ -0,0 +1,13 @@
+let _homeservice = null;
+
+class HomeController {
+    constructor({ HomeService }) {
+        _homeservice = HomeService;
+    }
+
+    index(req, res) {
+        return res.send(_homeservice.index());
+    }
+}
+
+module.exports = HomeController;
