eaa40c3eeb4c62257daf5246fb271f02495f205a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-02-25 20:58:03 -0500

    [ADD]  mongoose-autopopulate dependency added

---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    User: require('./users.model'),
+    Idea: require('./idea.model'),
+    Comment: require('./comment.model')
+};
--- src/starup/container.js
+++ src/starup/container.js
@@ -1,8 +1,8 @@
 const { createContainer, asClass, asValue, asFunction } = require("awilix");
 
 // config
-const config = require('../config');
-const app = require('.');
+const config = require("../config");
+const app = require(".");
 
 // services
 const { HomeService } = require("../services");
@@ -13,6 +13,9 @@ const { HomeController } = require("../controllers");
 // routes
 const { HomeRoutes } = require("../routes/index.routes");
 const Routes = require("../routes");
+
+// models
+const { User, Idea, Comment } = require("../models");
 
 const container = createContainer();
 
@@ -30,6 +33,11 @@ container
   })
   .register({
     HomeRoutes: asFunction(HomeRoutes).singleton()
+  })
+  .register({
+    User: asValue(User),
+    Idea: asValue(Idea),
+    Comment: asValue(Comment)
   });
 
 module.exports = container;
