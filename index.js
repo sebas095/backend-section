05fa44e4f5e0ae2dafe975be312357bf617df92c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-02-25 22:11:11 -0500

    [ADD]  useFindAndModify modify to mongoose config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.set("useCreateIndex", true);
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
     useUnifiedTopology: true
   })
   .then(() => server.start())
